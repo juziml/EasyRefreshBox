68872b7c18e4e49a71b168a8fc294bc30e99a020
rango <sink50@foxmail.com>
2021-04-05 18:08:48 +0800

    -上拉，下拉骨架

--- app/src/main/java/com/rg/eb/EasyRefreshBox.kt
+++ app/src/main/java/com/rg/eb/EasyRefreshBox.kt
@@ -6,6 +6,7 @@ import android.view.MotionEvent
 import android.view.View
 import android.widget.FrameLayout
 import android.widget.TextView
+import kotlin.math.abs
 
 /**
  *@Desc:
@@ -14,37 +15,44 @@ import android.widget.TextView
  *create by zhusw on 4/5/21 14:21
  */
 class EasyRefreshBox:FrameLayout {
-    constructor(context: Context):this(context,null){
+    constructor(context: Context) : this(context, null) {
         "constructor 1 ".log()
     }
-    constructor(context: Context,attributeSet: AttributeSet?):super(context,attributeSet){
+
+    constructor(context: Context, attributeSet: AttributeSet?) : super(context, attributeSet) {
         "constructor 2 ".log()
 
     }
+
     private lateinit var contentView: View
     private lateinit var tvState: TextView
     override fun onFinishInflate() {
         super.onFinishInflate()
         contentView = findViewById(R.id.v_content)
         tvState = findViewById(R.id.tv_refreshState)
+        tvState.tag = PullState.UN_EFFECT
     }
 
     init {
         "init ".log()
     }
 
-    private var downY:Float = 0F
+    private val MIN_EFFECT_PULL_DOWN_Y = 20.dp
+
+    private val MAX_PULL_DOWN_Y = 100.dp
+
+    private var downY: Float = 0F
     override fun onTouchEvent(event: MotionEvent): Boolean {
 
-        when(event.action){
-            MotionEvent.ACTION_DOWN->{
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
                 downY = event.y
             }
-            MotionEvent.ACTION_MOVE ->{
-                val moveY  = event.y - downY
+            MotionEvent.ACTION_MOVE -> {
+                val moveY = event.y - downY
                 moveContentView(moveY)
             }
-            MotionEvent.ACTION_UP->{
+            MotionEvent.ACTION_UP -> {
                 downY = 0F
                 moveContentView(0F)
             }
@@ -52,12 +60,29 @@ class EasyRefreshBox:FrameLayout {
         return true
     }
 
+    private var grandTotal: Float = 0F
+
     /**
      * 使用translation 直接操作内容view
      * 这样只需要操作内容view的位置，而不用关心与其他同级view的状态
      */
-    private fun moveContentView(y: Float){
+    private fun moveContentView(y: Float) {
         contentView.translationY = y
+        grandTotal = y
+
+
     }
+
+    private fun handlerStatus(distance:Float){
+        val absDistance = Math.abs(distance)
+        val lastState:PullState = tvState.tag as PullState
+
+    }
+
+}
+enum class PullState(val v:Int){
+    UN_EFFECT(0),
+    HOLDING(1),
+    EFFECT(2),
 
 }
--- app/src/main/java/com/rg/eb/Extensions.kt
+++ app/src/main/java/com/rg/eb/Extensions.kt
@@ -1,5 +1,8 @@
 package com.rg.eb
 
+import android.content.res.Resources
+import android.util.TypedValue
+
 /**
  *@Desc:
  *-
@@ -10,3 +13,8 @@ private val TAG = "EasyRefreshBox"
 fun String.log(secondTag:String=""){
     println("$TAG:$secondTag$this")
 }
+val Float.dp
+get() = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,this,Resources.getSystem()
+    .displayMetrics)
+val Int.dp:Float
+get() = this.toFloat().dp
