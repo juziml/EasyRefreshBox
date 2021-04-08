80f01a08c88ca6834903760797f9fbcbe9222592
rango <sink50@foxmail.com>
2021-04-08 15:22:18 +0800

    -

--- app/src/main/java/com/rg/eb/NestedScrollingSimpleView.kt
+++ app/src/main/java/com/rg/eb/NestedScrollingSimpleView.kt
@@ -1,83 +1,107 @@
 package com.rg.eb
+
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.NestedScrollingParent3
+import android.widget.LinearLayout
+import androidx.core.view.NestedScrollingParent2
 import androidx.core.view.NestedScrollingParentHelper
 import androidx.core.view.ViewCompat
+import androidx.recyclerview.widget.RecyclerView
 
 /**
  *@Desc:
  *create by zhusw on 4/8/21 11:31
  */
-class NestedScrollingSimpleView(context: Context, attributeSet: AttributeSet):ConstraintLayout(context,attributeSet)
-        ,NestedScrollingParent3 {
-    private val nestedScrollingParentHelper by lazy {
-        NestedScrollingParentHelper(this)
-    }
-    init {
-    }
-    override fun onNestedScrollAccepted(child: View, target: View, axes: Int, type: Int) {
-        nestedScrollingParentHelper.onNestedScrollAccepted(child,target,axes,type)
+class NestedScrollingSimpleView(context: Context, attributeSet: AttributeSet) : LinearLayout(context, attributeSet),
+        NestedScrollingParent2 {
+    private lateinit var mHeader: View
+    private lateinit var mTarget: View
+    private var mHeaderHeight = 0
+
+    private val mNestedScrollingParentHelper by lazy { NestedScrollingParentHelper(this) }
+    private var rvScrollCount = 0F
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        if (childCount > 0) {
+            mHeader = getChildAt(0)
+            // 遍历出RecyclerView
+            for (i in 0 until childCount) {
+                if (getChildAt(i) is RecyclerView) {
+                    mTarget = getChildAt(i)
+                    break
+                }
+            }
+        }
+        val rv = mTarget as RecyclerView
+        rv.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                rvScrollCount+= dy
+            }
+        })
     }
 
-    override fun onNestedScrollAccepted(child: View, target: View, axes: Int) {
-        nestedScrollingParentHelper.onNestedScrollAccepted(child,target,axes)
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        mHeaderHeight = mHeader.measuredHeight
+        // 需要正确的测量出RecyclerView在header折叠时的高度，不然RecyclerView显示不全
+        mTarget.measure(
+                widthMeasureSpec,
+                MeasureSpec.makeMeasureSpec(
+                        mHeaderHeight + mTarget.measuredHeight,
+                        MeasureSpec.EXACTLY)
+        )
     }
 
     override fun onStartNestedScroll(child: View, target: View, axes: Int, type: Int): Boolean {
-        "NestedScrollingSimpleView onStartNestedScroll axex=$axes".log()
-            return true
+        // 允许竖直方向上的滑动
+        return isEnabled && (axes and ViewCompat.SCROLL_AXIS_VERTICAL != 0)
     }
 
-    override fun onStartNestedScroll(child: View, target: View, axes: Int): Boolean {
-        "NestedScrollingSimpleView onStartNestedScroll axex=$axes".log()
-        return true
+    override fun onNestedScrollAccepted(child: View, target: View, axes: Int, type: Int) {
+        // 代理给NestedScrollingParentHelper
+        mNestedScrollingParentHelper.onNestedScrollAccepted(child, target, axes, type)
     }
 
-    override fun onNestedFling(target: View, velocityX: Float, velocityY: Float, consumed: Boolean): Boolean {
-      return false
-    }
-
-    override fun getNestedScrollAxes(): Int {
-        return nestedScrollingParentHelper.nestedScrollAxes
-    }
-
+    //在响应嵌套滑动钱做一些预处理，比如读取子view滑动极限状态初始化
     override fun onNestedPreScroll(target: View, dx: Int, dy: Int, consumed: IntArray, type: Int) {
-        if (type == ViewCompat.TYPE_TOUCH) {
-            "NestedScrollingSimpleView onNestedPreScroll====1 dy=$dy".log()
-            onNestedPreScroll(target, dx, dy, consumed)
+
+        //能否继续下滑
+        val targetCanDown = target.canScrollVertically(-1)
+        //能否继续上滑
+        val targetCanUp = target.canScrollVertically(1)
+
+        // dy大于0说明在向上滑动，此时需要判断header是不是还能显示
+        val canScrollUp = dy > 0 && mHeaderHeight > scrollY
+        // dy小于0说明在向下滑动，此时需要判断header是不是已经显示完整
+        val canScrollDown = dy < 0 && rvScrollCount<=0F
+        if (canScrollUp || canScrollDown) {
+            scrollBy(0, dy)
+            consumed[1] = dy
         }
+        "NestedScrollingSimpleView onNestedPreScroll targetCanDown=$targetCanDown targetCanUp=$targetCanUp".log()
     }
 
-    override fun onNestedPreScroll(target: View, dx: Int, dy: Int, consumed: IntArray) {
-        "NestedScrollingSimpleView onNestedPreScroll=====2 dy=$dy".log()
-    }
+    override fun onNestedScroll(
+            target: View,
+            dxConsumed: Int,
+            dyConsumed: Int,
+            dxUnconsumed: Int,
+            dyUnconsumed: Int,
+            type: Int
+    ) {
 
-    override fun onNestedScroll(target: View, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int, type: Int, consumed: IntArray) {
-        "NestedScrollingSimpleView onNestedScroll dyUnconsumed=$dyUnconsumed".log()
-    }
 
-    override fun onNestedScroll(target: View, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int, type: Int) {
-        "NestedScrollingSimpleView onNestedScroll===2 dyUnconsumed=$dyUnconsumed".log()
-    }
-
-    override fun onNestedScroll(target: View, dxConsumed: Int, dyConsumed: Int, dxUnconsumed: Int, dyUnconsumed: Int) {
-        "NestedScrollingSimpleView onNestedScroll===3 dyUnconsumed=$dyUnconsumed".log()
-    }
-
-    override fun onNestedPreFling(target: View, velocityX: Float, velocityY: Float): Boolean {
-       return false
     }
 
     override fun onStopNestedScroll(target: View, type: Int) {
-       nestedScrollingParentHelper.onStopNestedScroll(target,type)
+        // 代理给NestedScrollingParentHelper
+        mNestedScrollingParentHelper.onStopNestedScroll(target, type)
     }
 
-    override fun onStopNestedScroll(target: View) {
-        nestedScrollingParentHelper.onStopNestedScroll(target)
+    override fun scrollTo(x: Int, y: Int) {
+        val clampY = Math.max(0, Math.min(y, mHeaderHeight))
+        super.scrollTo(x, clampY)
     }
-
 
 }
